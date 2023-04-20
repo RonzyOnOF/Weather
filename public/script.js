ae5919a01bc61ef51755d20dc37c2d2288fc3b53
Ronzy <ninjakai25@gmail.com>
2023-04-20 13:36:13 -0500

    finish get weather request

--- public/script.js
+++ public/script.js
@@ -1,32 +1,62 @@
 const apiKey = '902022d14a2ccd623e1966f211a3c908';
-const url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
-const param = `${url}${apiKey}`;
 const submit = document.querySelector(".submit");
+
 let input = document.querySelector("input");
 const image = document.querySelector("img");
 const temp = document.getElementById("temperature");
 const condition = document.getElementById("weather-condition");
-
+let stats = [];
 
 
 
 const getWeather = async () => {
     const location = input.value;
-    console.log(location);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
+
     
-    // try {
-    //     const response = await fetch(param);
-    //     if (response.ok) {
-    //         const jsonResponse = await response.json();
-    //         const location = 
-    //         console.log('Succesfuly retrieved weather data');
-    //         console.log(jsonResponse.city);
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    // }
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log('Succesfuly retrieved weather data');
+            // console.log(jsonResponse.weather[0].main);
+            // console.log(jsonResponse.main.temp);
+            const weather = jsonResponse.weather[0].main;
+            const temp = jsonResponse.main.temp;
+            stats.push(weather);
+            stats.push(temp);
+            return stats;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+
 }
 
-submit.addEventListener('click', () => {
-    getWeather();
+// const weatherLogo = (weather) => {
+//     switch (weather) {
+//         case 'Clouds':
+//             image.src = 'cloud.png';
+//             image.style.display = 'block';
+//             break;
+        
+//         default:
+//             break;
+//     }
+// }
+
+
+
+submit.addEventListener('click', async () => {
+    const location = await getWeather();
+    // console.log(stats);
+    console.log(stats[0]);
+    // weatherLogo(stats[0]);
+
+
 })
+
+
+
+
