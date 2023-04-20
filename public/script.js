9145c03dacac2028a77b6201726cbe6bc0e77b34
JohnnyBoi <ninjakai25@gmail.com>
2023-04-20 17:55:19 -0500

    add function to display right image

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ let stats = [];
 const getWeather = async () => {
     const location = input.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
-
+    stats = [];
     
     try {
         const response = await fetch(url);
@@ -34,25 +34,71 @@ const getWeather = async () => {
 
 }
 
-// const weatherLogo = (weather) => {
-//     switch (weather) {
-//         case 'Clouds':
-//             image.src = 'cloud.png';
-//             image.style.display = 'block';
-//             break;
-        
-//         default:
-//             break;
-//     }
-// }
+
+const kelvinToF = (temp) => {
+    return (temp - 237.15) * 9/5 + 32;
+}
+
+const kelvinToC = (temp) => {
+    return Math.round(temp - 237.15);
+}
+
+
+const weatherLogo = (weather) => {
+    switch (weather[0]) {
+        case 'Clouds':
+            image.src = 'resources/images/cloud.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        case 'Rain':
+            image.src = 'resources/images/rain.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        case 'Haze':
+            image.src = 'resources/images/cloud.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        case 'Thunderstorm':
+            image.src = 'resources/images/snow.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        case 'Clear':
+            image.src = 'resources/images/clear.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        case 'Snow':
+            image.src = 'resources/images/snow.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        case 'Mist':
+            image.src = 'resources/images/mist.png';
+            image.style.display = 'block';
+            temp.innerHTML = `${weather[1]}°F`;
+            condition.innerHTML = weather[0];
+            break;
+        default:
+            break;
+    }
+}
 
 
 
 submit.addEventListener('click', async () => {
     const location = await getWeather();
-    // console.log(stats);
+    weatherLogo(stats);
     console.log(stats[0]);
-    // weatherLogo(stats[0]);
 
 
 })
